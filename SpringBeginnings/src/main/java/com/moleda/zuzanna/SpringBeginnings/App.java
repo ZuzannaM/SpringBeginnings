9e00f6264916108db397b54eb98330860d3291b8
ZuzannaM <zuuz94@gmail.com>
2017-02-23 21:37:55 +0100

    Spring startup - dependencies.

---
+++ SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/App.java
@@ -0,0 +1,13 @@
+package com.moleda.zuzanna.SpringBeginnings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
