3aafd6fcc5089582f7a7d4686713d757acf2183b
ZuzannaM <zuuz94@gmail.com>
2017-02-23 23:15:32 +0100

    Getting a bean Triangle from ApplicationContext using property tag or constructor-arg tag.

--- SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/Triangle.java
+++ SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/Triangle.java
@@ -2,7 +2,39 @@ package com.moleda.zuzanna.SpringBeginnings;
 
 public class Triangle {
 
-	public void draw(){
-		System.out.println("Triangle drawn.");
+	private String type;
+	private Integer height;
+	
+	public Triangle(Integer height) {
+		this.height = height;
+	}
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public Triangle(String type, Integer height) {
+		this.type = type;
+		this.height = height;
+	}
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	public void setHeight(Integer height) {
+		this.height = height;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public void draw() {
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
 	}
 }
