cb6fa27f9172374cd7be264c879e9bdabc0ae5c9
ZuzannaM <zuuz94@gmail.com>
2017-02-25 13:09:21 +0100

    Lifecycle callbacks.
    Using initializing and disposable Spring interfaces and my own methods.

--- SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/App.java
+++ SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/App.java
@@ -4,21 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 	
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		context.registerShutdownHook();
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
-		
-		// ERROR bean id="parentTriangle" is defined abstract
-		//Triangle triangle2 = (Triangle) context.getBean("parentTriangle");
-		//triangle2.draw();
-		
-		
 		
 	}
 }
--- SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/Triangle.java
+++ SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/Triangle.java
@@ -2,7 +2,10 @@ package com.moleda.zuzanna.SpringBeginnings;
 
 import java.util.List;
 
-public class Triangle {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class Triangle implements InitializingBean, DisposableBean{
 
 	private Point pointA;
 	private Point pointB;
@@ -36,5 +39,24 @@ public class Triangle {
 		System.out.println("PointA = ( " + getPointA().getX() + ", " + getPointA().getY() + " )");
 		System.out.println("PointB = ( " + getPointB().getX() + ", " + getPointB().getY() + " )");
 		System.out.println("PointC = ( " + getPointC().getX() + ", " + getPointC().getY() + " )");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("DisposableBean destroy method called by Triangle");
+		
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("InitializingBean init method called by Triangle");
+	}
+	
+	public void myInit(){
+		System.out.println("MyInit method called by Triangle");
+	}
+	
+	public void cleanUp(){
+		System.out.println("CleanUp method called by Triangle");
 	}
 }
