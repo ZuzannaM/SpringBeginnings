cb102c17f21eaac62c44ad0be02a658873a43b3c
ZuzannaM <zuuz94@gmail.com>
2017-02-25 13:40:09 +0100

    BeanPostProcessor.

---
+++ SpringBeginnings/src/main/java/com/moleda/zuzanna/SpringBeginnings/DisplayNameBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package com.moleda.zuzanna.SpringBeginnings;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class DisplayNameBeanPostProcessor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In after inicialization process method - bean name is " + beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("In before inicialization process method - bean name is " + beanName);
+		return bean;
+	}
+
+}
